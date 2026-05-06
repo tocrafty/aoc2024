f451b16d2274ef0dfb6ecaee3c610512dd78e952
cooperyan <cooperyan@tencent.com>
2026-05-07 00:18:14 +0800

    25Y day 10 part 2

--- aoc2025/Day10.hs
+++ aoc2025/Day10.hs
@@ -11,21 +11,100 @@ import qualified Data.Array as A
 import Debug.Trace
 import Data.Hashable
 import Data.Ord
+import qualified Data.Array.ST as MA
+
+trace' = const id
 
 main :: IO ()
 main = do
   content <- readFile "aoc2025/input"
   let machines = fromJust $ evalStateT (runParser parseInput) content
-  --print $ sum $ (\(target, ts, _) -> minToggles (fmap replToggle ts) target) <$> machines
-  --print $ sum $ fmap (\(_, tss, js) -> trace ("tss: " ++ show tss ++ "js: " ++ show js) $ runST $ do
-  --  let end = length js - 1
-  --  ht <- HT.new
-  --  HT.insert ht (A.listArray (0, end) $ take (end+1) $ repeat 0) 0
-  --  bfs ht (A.listArray (0, end) js) tss 1
-  --  ) $ machines
-  print $ sum $ catMaybes $ fmap (\(_, tss, js) -> trace (show tss ++ " " ++ show js) $ minToggle tss (list2Array js)) $ machines
+  print $ sum $ (\(target, ts, _) -> minToggles (fmap replToggle ts) target) <$> machines
+  --print $ fmap (\(_, tss, js) -> minToggle tss (list2Array js)) $ machines
+  print $ sum $ catMaybes $ fmap (
+    \(_, toggles, joltages) -> runST $ do
+      trace' (show joltages) $ return ()
+      (p2ts, cache) <- parity2Combs toggles
+      t0s <- MA.readArray p2ts 0
+      minToggleByParity p2ts cache (list2Array joltages) (concat <$> ([]:allCombs (filter (not.null) t0s)))
+    ) $ machines
  where
   list2Array xs = A.listArray (0, length xs - 1) xs
+
+-- another method is math way. Solve free matrix first and get the result.
+
+-- see https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+minToggleByParity :: MA.STArray s Int [[Toggle]] -> HT.HashTable s (A.Array Int Int) (Maybe Int) -> A.Array Int Int -> [[Toggle]] -> ST s (Maybe Int)
+minToggleByParity p2ts cache joltages combsOf0 = do
+  n' <- HT.lookup cache joltages
+  case n' of
+    Just n -> return n
+    _ -> let pj = parityOf joltages in
+      if pj == 0 then do
+        minToggles <- sequence $ fmap (
+          \ts -> do
+            let js = remain ts
+            if (any (<0) js) then trace' ("invalid: " ++ show (A.elems js) ++ " tss: " ++ show ts) $ return Nothing
+            else do
+              r <- minToggleByParity p2ts cache (fmap (`div` 2) js) combsOf0
+              trace' ("valid: " ++ show (A.elems $ fmap (`div` 2) js) ++ " tss: " ++ show ts ++ " r: " ++ show r) $ return ()
+              return $ fmap ((+ length ts) . (* 2)) r
+          ) combsOf0
+        case catMaybes minToggles of
+          [] -> do
+            trace' ("invalid: " ++ show (A.elems joltages)) $ HT.insert cache joltages Nothing
+            return Nothing
+          xs -> do
+            let r = minimum xs
+            trace' ("joltages: " ++ show (A.elems joltages) ++ " pj: " ++ show pj ++ " pj == 0: " ++ " min: " ++ show r) $
+              HT.insert cache joltages (Just r)
+            return $ Just r
+      else do
+        tss <- MA.readArray p2ts pj
+        minToggles <- sequence $ fmap (
+          \ts -> let js = remain ts in if (any (<0) js) then return Nothing else do
+            r <- minToggleByParity p2ts cache js combsOf0
+            return $ (length ts + ) <$> r
+          ) tss
+        case catMaybes minToggles of
+          [] -> do
+            trace' ("invalid: " ++ show (A.elems joltages)) $ HT.insert cache joltages Nothing
+            return Nothing
+          xs -> do
+            let r = minimum xs
+            trace' ("joltages: " ++ show (A.elems joltages) ++ " pj: " ++ show pj ++ " ts: " ++ show tss ++ " min: " ++ show r) $
+              HT.insert cache joltages (Just r)
+            return $ Just r
+ where
+  minus t toggle = (A.//) t $ fmap (\i -> (i, (A.!) t i - 1)) toggle
+  remain toggles = foldl' (minus) joltages toggles
+
+parityOf :: Integral a => A.Array i a -> a
+parityOf joltages = sum $ uncurry (*) <$> zip ((2^) <$> [0..]) ((`mod` 2) <$> A.elems joltages)
+
+type Toggle = [Int]
+
+parity2Combs :: [Toggle] -> ST s (MA.STArray s Int [[Toggle]], HT.HashTable s (A.Array Int Int) (Maybe Int))
+parity2Combs toggles = do
+  p2ts <- MA.newArray (0, maxParity) []
+  cache <- HT.new
+  HT.insert cache zeros (Just 0)
+  sequence_ $ fmap (\toggles -> do
+    let p = parityOf (addAll toggles)
+    if p == 0 then trace' ("zero: " ++ show toggles) $ return () else return ()
+    MA.modifyArray p2ts p (toggles :)
+    ) $ allCombs toggles
+  return $ (p2ts, cache)
+ where
+  maxIdx = maximum (fmap maximum toggles)
+  maxParity = 2 ^ (1 + maxIdx) - 1
+  add joltages toggle = (A.//) joltages $ fmap (\i -> (i, (A.!) joltages i + 1)) toggle
+  zeros = A.listArray (0, maxIdx) $ take (maxIdx + 1) $ repeat 0
+  addAll = foldl' add zeros
+
+allCombs :: [a] -> [[a]]
+allCombs [] = [[]]
+allCombs (x:xs) = allCombs xs ++ fmap (x:) (allCombs xs)
 
 minToggles :: [Int] -> Int -> Int
 minToggles ts target = runST $ do
@@ -44,9 +123,9 @@ dfs ht ts c l = do
                  else return ()
     ) $ (l `xor`) <$> ts
 
+-- it's still very slow with many prune method. 
 minToggle :: [[Int]] -> A.Array Int Int -> Maybe Int
 minToggle tss' target =
-  -- trace ("minToggle: " ++ show tss ++ " --- " ++ show (A.elems target)) $
   if any (<0) target then Nothing
   else if all (==0) target then Just 0
   else if length toggles == 0 then Nothing
@@ -56,15 +135,15 @@ minToggle tss' target =
  where
   zeros = filter ((== 0) . (A.!) target) (A.indices target)
   tss = filter (not . flip containsAny zeros) tss'
-  containsAny xs as = any id $ fmap ($ xs) $ elem <$> as
+  containsAny xs as = any (`elem` xs) as
   (b, toggles) = minimumBy (comparing (\(x, bs) -> (length bs, (A.!) target x))) $ fmap (\x -> (x, toggleWithButton x)) (filter ((/= 0) . (A.!) target) (A.indices target))
   toggleWithButton x = filter (elem x) tss
   toggleBounds = (minimum . fmap ((A.!) target)) <$> toggles
   allCases = zip toggles <$> distribute' ((A.!) target b) toggleBounds
   reduceJoltage j ts n = (A.//) j $ (\x -> (x, (A.!) j x - n)) <$> ts
-  reduceJoltages = foldl (\j (ts, n) -> reduceJoltage j ts n) target
+  reduceJoltages = foldl' (\j (ts, n) -> reduceJoltage j ts n) target
   minToggle' jns =
-    (if b == 7 then trace ("tss: " ++ show tss ++ " target: " ++ show (A.elems target) ++ " b: " ++ show b ++ " jns: " ++ show jns) else id) $
+    (if b == -1 then trace' ("tss: " ++ show tss ++ " target: " ++ show (A.elems target) ++ " b: " ++ show b ++ " jns: " ++ show jns) else id) $
     minToggle tss $ reduceJoltages jns
 
 distribute' :: Int -> [Int] -> [[Int]]
@@ -104,7 +183,7 @@ bfs ht target tss n = do
 dfs' :: HT.HashTable s (A.Array Int Int) (Maybe Int) -> A.Array Int Int -> [[Int]] -> ST s (Maybe Int)
 dfs' ht joltages tss
   | any (<0) joltages = return Nothing
-  | otherwise = trace ("joltages: " ++ show joltages) $ do
+  | otherwise = do
     mmc <- HT.lookup ht joltages
     case mmc of
       Just mc -> return mc
